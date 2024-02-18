b4ba87533c2ade35046388dd3e46b20addb50d36
vsabouroux <vsabouroux@aol.com>
2024-02-18 13:26:35 +0100

    configuration server

--- vs-portfolio/backend/app.js
+++ vs-portfolio/backend/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://sabourouxvalerie:Escpau3364!@cluster0.ivhsztv.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 const app = express();
 
 app.use((req, res) => {
