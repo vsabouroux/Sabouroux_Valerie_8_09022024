bd7159b25741a333bde3bc8125fbcb8175a426ce
vsabouroux <vsabouroux@aol.com>
2024-02-22 19:08:11 +0100

    Insertion logos github et linkedin mais pb d'affichage (plugin babel ?)

--- vs-portfolio/frontend/src/components/Footer/Footer.js
+++ vs-portfolio/frontend/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
-// import vslogoSmall from "../../assets/vs-logoSmall.png";
+import githubLogo from "../../assets/github.svg";
+import linkedinLogo from "../../assets/linkedin-in.svg";
 import "./Footer.scss";
 
 function Footer() {
@@ -7,13 +8,30 @@ function Footer() {
       {/* <div className="bandeau">
         <img src={vslogoSmall} alt="logo vs" className="vs-small-logo" />
       </div> */}
-           <div className="footerElements">
-          <div className="realisation">
-        <p>2024 - Réalisé par Valérie Sabouroux</p>
-      </div>
-      <div className="mentionsLegales">
-        <p>Mentions légales</p>
-      </div>
+      <div className="footerElements">
+        <div className="realisation">
+          <p>2024 - Réalisé par Valérie Sabouroux</p>
+        </div>
+        {/* problème liens github et linkedin Les logos n'apparaissent pas sur PA ! */}
+        <div className="liens">
+          <a
+            href="https://github.com/vsabouroux/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubLogo} alt="logo GitHub" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/vsabouroux/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinLogo} alt="logo réseau social LinkedIn" />
+          </a>
+        </div>
+        <div className="mentionsLegales">
+          <p>Mentions légales</p>
+        </div>
       </div>
     </footer>
   );
