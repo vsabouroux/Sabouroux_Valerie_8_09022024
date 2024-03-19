8a02fe8ff6e61275358173fd71a0c6d7764df856
vsabouroux <vsabouroux@aol.com>
2024-03-19 10:46:57 +0100

    connexion du backend à la BDD MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sabourouxvalerie:cFzcdyYirbgVjCFe@cluster0.ivhsztv.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://sabourouxvalerie:DFcWeb4moOAgCObE@cluster0.ivhsztv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 const app = express();
 
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue test portolio !' }); 
+  res.json({ message: "Votre requête a bien été reçue test portolio !" });
 });
 
 module.exports = app;
