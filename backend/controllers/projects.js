948414dc1997c94a0dec4309e0507a20c42c2675
vsabouroux <vsabouroux@aol.com>
2024-04-30 18:52:40 +0200

    Update backend "projects.js"

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -61,56 +61,75 @@ exports.getOneProject = (req, res, next) => {
 
 exports.modifyProject = (req, res, next) => {
   let projectData = req.body.projet; // Récupérez directement les données du projet depuis req.body
- // Vérifier si des données de projet sont fournies dans la requête
- 
- if (!projectData) {
-  return res.status(400).json({ message: "Aucune donnée de projet fournie." });
-}
+  // Vérifier si des données de projet sont fournies dans la requête
 
-// Analyser les données du projet pour déterminer quels champs doivent être mis à jour
-let updatedFields = {};
-try {
-  updatedFields = JSON.parse(projectData);
-} catch (error) {
-  return res.status(400).json({ error: "Les données de projet ne sont pas au format JSON valide." });
-}
-
-// Vérifier si un nouveau fichier a été téléchargé pour remplacer l'image existante
-if (req.file) {
-  updatedFields.imageUrl = `${req.protocol}://${req.get("host")}/images/${req.file.filename}`;
-// Supprime l'image existante
-Project.findById(req.params.id)
-.then((project) => {
-  if (!project) {
-    return res.status(404).json({ message: "Projet non trouvé !" });
+  if (!projectData) {
+    return res
+      .status(400)
+      .json({ message: "Aucune donnée de projet fournie." });
   }
-  if (project.imageUrl) {
-    const imagePath = path.join(
-      __dirname,
-      "../images/",
-      project.imageUrl.split("/images/")[1]
-    );
-    fs.unlink(imagePath, (err) => {
-      if (err) {
-        console.error("Erreur lors de la suppression de l'image existante :", err);
+
+  // Analyser les données du projet pour déterminer quels champs doivent être mis à jour
+  let updatedFields = {};
+  try {
+    updatedFields = JSON.parse(projectData);
+  } catch (error) {
+    return res
+      .status(400)
+      .json({
+        error: "Les données de projet ne sont pas au format JSON valide.",
+      });
+  }
+  // Vérifier si les tags et les skills sont fournis et les transformer en tableaux si nécessaire
+  if (updatedFields.tags && typeof updatedFields.tags === 'string') {
+    updatedFields.tags = updatedFields.tags.split(",");
+  }
+  if (updatedFields.skills && typeof updatedFields.skills === 'string') {
+    updatedFields.skills = updatedFields.skills.split(",");
+  }
+  // Vérifier si un nouveau fichier a été téléchargé pour remplacer l'image existante
+  if (req.file) {
+    updatedFields.imageUrl = `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`;
+    // Supprime l'image existante
+    Project.findById(req.params.id)
+      .then((project) => {
+        if (!project) {
+          return res.status(404).json({ message: "Projet non trouvé !" });
+        }
+        if (project.imageUrl) {
+          const imagePath = path.join(
+            __dirname,
+            "../images/",
+            project.imageUrl.split("/images/")[1]
+          );
+          fs.unlink(imagePath, (err) => {
+            if (err) {
+              console.error(
+                "Erreur lors de la suppression de l'image existante :",
+                err
+              );
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la recherche du projet :", error);
+      });
+  }
+
+  // Mettre à jour le projet seulement avec les champs fournis dans la requête
+  Project.findByIdAndUpdate(req.params.id, updatedFields, { new: true })
+    .then((updatedProjet) => {
+      if (!updatedProjet) {
+        return res.status(404).json({ message: "Projet non trouvé !" });
       }
-    });
-  }
-})
-.catch((error) => {
-  console.error("Erreur lors de la recherche du projet :", error);
-});
-}
-
-// Mettre à jour le projet seulement avec les champs fournis dans la requête
-Project.findByIdAndUpdate(req.params.id, updatedFields, { new: true })
-  .then((updatedProjet) => {
-    if (!updatedProjet) {
-      return res.status(404).json({ message: "Projet non trouvé !" });
-    }
-    res.status(200).json({ message: "Projet modifié !", projet: updatedProjet });
-  })
-  .catch((error) => res.status(500).json({ error }));
+      res
+        .status(200)
+        .json({ message: "Projet modifié !", projet: updatedProjet });
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.deleteProject = (req, res, next) => {
