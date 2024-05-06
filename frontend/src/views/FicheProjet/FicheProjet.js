b6a75d4d3e9fd865286214f26fe7a1c2995353c5
vsabouroux <vsabouroux@aol.com>
2024-05-06 14:52:58 +0200

    Amélioration accessibilité
    Texte alternatif pour image projet

--- frontend/src/views/FicheProjet/FicheProjet.js
+++ frontend/src/views/FicheProjet/FicheProjet.js
@@ -60,9 +60,6 @@ const FicheProjet = ({ projets }) => {
 
   const {title, description, skills, tags, githubUrl, imageUrl } = projet;
 
-  // Construction de l'URL complète de l'image. J'ai enlevé ".imageUrl" ds les {} ci-dessous
-  // const imageUrl =`https://vs-portfolio-api-0c0adab5969b.herokuapp.com/images/${projet.imageUrl}`;
-
   return (
     <div>
       <main>
